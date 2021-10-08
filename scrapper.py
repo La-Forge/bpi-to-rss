cb47c4e92843efb09ff6a01fc5404d022d734c91
Vincent LEMELTIER <vincent.lemeltier.ae@gmail.com>
2021-10-08 11:46:37 +0200

    fix path

--- scrapper.py
+++ scrapper.py
@@ -5,11 +5,11 @@ from scrappers.gnius import GniusScrapper
 from scrappers.bpi import BpiScrapper
 import sys
 from http.server import HTTPServer, BaseHTTPRequestHandler
-
 import sentry_sdk
+from sentry_sdk import capture_exception
+
 sentry_sdk.init(
     "https://050cb1f4aff04d22af23721245c4ae35@o1031661.ingest.sentry.io/5998395",
-
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
@@ -24,9 +24,9 @@ class RssRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self._set_headers()
         rss = '<?xml version="1.0" encoding="UTF-8"?><xml></xml>'.encode('utf8')
-        if(self.path=="/bpi"):
+        if(self.path[0:4]=="/bpi"):
             rss = self.bpi_scrapper.generate_feed(verbose=False)
-        elif(self.path=="/gnius"):
+        elif(self.path[0:6]=="/gnius"):
             rss = self.gnius_scrapper.generate_feed(verbose=False)
 
         self.wfile.write(rss)
@@ -36,14 +36,16 @@ class RssRequestHandler(BaseHTTPRequestHandler):
 
 
 def main(server_class=HTTPServer, handler_class=RssRequestHandler, addr="localhost", port=8000):
-    handler_class.bpi_scrapper = BpiScrapper()
-    handler_class.gnius_scrapper = GniusScrapper()
-    server_address = (addr, port)
-    httpd = server_class(server_address, handler_class)
-    
-    print(f"Starting httpd server on {addr}:{port}")
-    httpd.serve_forever()
-
+    try:
+        handler_class.bpi_scrapper = BpiScrapper()
+        handler_class.gnius_scrapper = GniusScrapper()
+        server_address = (addr, port)
+        httpd = server_class(server_address, handler_class)
+        
+        print(f"Starting httpd server on {addr}:{port}")
+        httpd.serve_forever()
+    except Exception as e:
+        capture_exception(e)
 
     
 if __name__ == "__main__":
