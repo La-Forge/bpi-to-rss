1b2038d78ce9d8f1f0caabf66f908b4008a1d207
ctricot <christophe@la-forge.ai>
2023-03-25 16:49:44 +0100

    MISC clean code

--- scrapper.py
+++ scrapper.py
@@ -1,10 +1,9 @@
 
-
-
 from scrappers.gnius import GniusScrapper
 from scrappers.bpi import BpiScrapper
 import sys
 from http.server import HTTPServer, BaseHTTPRequestHandler
+
 
 class RssRequestHandler(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -14,10 +13,11 @@ class RssRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
-        rss = '<?xml version="1.0" encoding="UTF-8"?><xml></xml>'.encode('utf8')
-        if(self.path[0:4]=="/bpi"):
+        rss = '<?xml version="1.0" encoding="UTF-8"?><xml></xml>'.encode(
+            'utf8')
+        if (self.path[0:4] == "/bpi"):
             rss = self.bpi_scrapper.generate_feed(verbose=False)
-        elif(self.path[0:6]=="/gnius"):
+        elif (self.path[0:6] == "/gnius"):
             rss = self.gnius_scrapper.generate_feed(verbose=False)
 
         self.wfile.write(rss)
@@ -27,16 +27,14 @@ class RssRequestHandler(BaseHTTPRequestHandler):
 
 
 def main(server_class=HTTPServer, handler_class=RssRequestHandler, addr="localhost", port=8000):
+    handler_class.bpi_scrapper = BpiScrapper()
+    handler_class.gnius_scrapper = GniusScrapper()
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
 
-        handler_class.bpi_scrapper = BpiScrapper()
-        handler_class.gnius_scrapper = GniusScrapper()
-        server_address = (addr, port)
-        httpd = server_class(server_address, handler_class)
-        
-        print(f"Starting httpd server on {addr}:{port}")
-        httpd.serve_forever()
+    print(f"Starting httpd server on {addr}:{port}")
+    httpd.serve_forever()
 
 
-    
 if __name__ == "__main__":
     main(port=int(sys.argv[1]))
--- scrappers/gnius.py
+++ scrappers/gnius.py
@@ -1,4 +1,3 @@
-from datetime import date
 from html import unescape
 import requests
 from bs4 import BeautifulSoup
