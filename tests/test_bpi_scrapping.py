bb21f02162e8d4ce177958851271031b407b7710
Laurent Dosdat <laurent@la-forge.ai>
2024-04-02 15:19:09 +0200

    fix: updated BPI scrapping selectors to follow the evolution of the website
    - code was reformated using ruff
    - the page parameter needed to be in the URL, not in the POST body
    - all extractions (title, link, etc.) are in separate methods with unit tests

---
+++ tests/test_bpi_scrapping.py
@@ -0,0 +1,55 @@
+"""
+2024.04.02 - v0.1 - Initial version
+
+"""
+
+import pytest
+from bs4 import BeautifulSoup
+
+from scrappers.bpi import BpiScrapper
+
+
+@pytest.fixture
+def bpi_data_soup():
+    with open("tests/bpi_data.html", "r") as f:
+        html = f.read()
+        return BeautifulSoup(html, "html.parser")
+
+
+@pytest.fixture
+def bpi_scrapper():
+    return BpiScrapper()
+
+
+@pytest.fixture
+def articles(bpi_scrapper, bpi_data_soup):
+    return bpi_scrapper.get_articles(bpi_data_soup)
+
+
+# write a pytest test that loads the html file "bpi_data.html" and checks if the data is correct
+def test_get_articles(bpi_data_soup, bpi_scrapper):
+    articles = bpi_scrapper.get_articles(bpi_data_soup)
+    assert len(articles) == 9
+
+
+def test_get_article_title_link(bpi_data_soup, bpi_scrapper):
+    articles = bpi_scrapper.get_articles(bpi_data_soup)
+    assert bpi_scrapper.get_article_title_and_link(articles[0]) == (
+        "Appel à Projets France-Chili –Greentech",
+        "https://www.bpifrance.fr/nos-appels-a-projets-concours/appel-a-projets-france-chili-greentech",
+    )
+
+
+def test_get_start_date(bpi_data_soup, bpi_scrapper):
+    articles = bpi_scrapper.get_articles(bpi_data_soup)
+    assert bpi_scrapper.get_start_date(articles[0]).strftime("%Y-%m-%d") == "2023-11-28"
+
+
+def test_get_article_type(bpi_data_soup, bpi_scrapper):
+    articles = bpi_scrapper.get_articles(bpi_data_soup)
+    assert bpi_scrapper.get_article_type(articles[0]) == "Appels à projets"
+
+
+def test_get_article_content(articles, bpi_scrapper):
+    content = bpi_scrapper.get_article_content(articles[0])
+    assert len(content) == 153
