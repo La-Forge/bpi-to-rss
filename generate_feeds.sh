89c7027dbcb04de6457156188e39a11af932c258
Laurent Dosdat <laurent@la-forge.ai>
2024-09-02 10:35:59 +0200

    FIX scrapping script runs from anywhere

--- generate_feeds.sh
+++ generate_feeds.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# cd to the directory of this file
+cd $(dirname "$0")
+
+# activate the virtual env
 source .venv/bin/activate
 
+# generate the RSS feeds
 python3 generate_feeds.py
 
+# deactivate the virtual env
 deactivate
