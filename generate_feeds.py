a9c1f2deec9e841a93db39e6fe3206ee6087540e
ctricot <christophe@la-forge.ai>
2024-09-12 08:27:45 +0200

    NEW add option to update one feed only

--- generate_feeds.py
+++ generate_feeds.py
@@ -3,7 +3,7 @@ from scrappers.bpi import BpiScrapper
 import os
 import argparse
 
-def main(verbose):
+def main(verbose, update_bpi, update_gnius):
     
     script_dir = os.path.dirname(os.path.abspath(__file__))
     feeds_dir = os.path.join(script_dir, 'feeds')
@@ -15,17 +15,25 @@ def main(verbose):
     bpi_feed_file = os.path.join(feeds_dir, 'bpi_feed.xml')
     gnius_feed_file = os.path.join(feeds_dir, 'gnius_feed.xml')
 
-    print(f"Updating {bpi_feed_file}...")
-    bpi_scrapper.update_feed_file(bpi_feed_file,verbose=verbose)
-    print(f"{bpi_feed_file} updated.")
+    if update_bpi:
+        print(f"Updating {bpi_feed_file}...")
+        bpi_scrapper.update_feed_file(bpi_feed_file,verbose=verbose)
+        print(f"{bpi_feed_file} updated.")
 
-    print(f"Updating {gnius_feed_file}...")
-    gnius_scrapper.update_feed_file(gnius_feed_file,verbose=verbose)
-    print(f"{gnius_feed_file} updated.")
+    if update_gnius:
+        print(f"Updating {gnius_feed_file}...")
+        gnius_scrapper.update_feed_file(gnius_feed_file,verbose=verbose)
+        print(f"{gnius_feed_file} updated.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Update feeds with optional verbosity.")
+    parser.add_argument('--bpifrance', action='store_true', help='Update only bpifrance feed')
+    parser.add_argument('--gnius', action='store_true', help='Update only gnius feed')
     parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
     args = parser.parse_args()
 
-    main(args.verbose)
+    # Si ni bpifrance ni gnius n'est spécifié, on met à jour les deux
+    update_bpi = args.bpifrance or (not args.bpifrance and not args.gnius)
+    update_gnius = args.gnius or (not args.bpifrance and not args.gnius)
+
+    main(args.verbose, update_bpi, update_gnius)
