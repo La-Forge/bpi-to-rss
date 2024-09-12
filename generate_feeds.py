617782da8d738a42339f140b650f1a17ce6097ec
ctricot <christophe@la-forge.ai>
2024-09-12 08:21:46 +0200

    MISC add verbose option updating the feeds

--- generate_feeds.py
+++ generate_feeds.py
@@ -1,8 +1,9 @@
 from scrappers.gnius import GniusScrapper
 from scrappers.bpi import BpiScrapper
 import os
+import argparse
 
-def main():
+def main(verbose):
     
     script_dir = os.path.dirname(os.path.abspath(__file__))
     feeds_dir = os.path.join(script_dir, 'feeds')
@@ -15,12 +16,16 @@ def main():
     gnius_feed_file = os.path.join(feeds_dir, 'gnius_feed.xml')
 
     print(f"Updating {bpi_feed_file}...")
-    bpi_scrapper.update_feed_file(bpi_feed_file)
+    bpi_scrapper.update_feed_file(bpi_feed_file,verbose=verbose)
     print(f"{bpi_feed_file} updated.")
 
     print(f"Updating {gnius_feed_file}...")
-    gnius_scrapper.update_feed_file(gnius_feed_file)
+    gnius_scrapper.update_feed_file(gnius_feed_file,verbose=verbose)
     print(f"{gnius_feed_file} updated.")
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Update feeds with optional verbosity.")
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
+    args = parser.parse_args()
+
+    main(args.verbose)
